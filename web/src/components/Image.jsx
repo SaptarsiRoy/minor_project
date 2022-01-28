fb6d263d444eda3ff4b39c6a12080963f1e5b2db
SaptarsiRoy <saptarsiroy12@gmail.com>
2022-01-29 00:17:27 +0530

    image component

---
+++ web/src/components/Image.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Image = (props) => (
+  <img
+    className={props.className}
+    src={props.src}
+    alt="start screen images"
+  />
+);
+
+export default Image;
