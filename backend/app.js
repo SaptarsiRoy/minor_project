74b703c30d828eab67abb1775e989202303d7846
SaptarsiRoy <saptarsiroy12@gmail.com>
2022-02-04 00:02:54 +0530

    added login and change /user to /signup

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 const userModel = require('./user.js');
 const recipeModel =  require('./recipes.js');
 
+
 const uri = "mongodb://localhost:27017/bhukkad"
+const saltRounds = 10;
 const app = express();
 
 mongoose.connect(uri);
@@ -34,34 +37,71 @@ app.get('/bhukkad/getrecipes', (req, res) => {
     })
 });
 
-app.post("/bhukkad/user", (req, res) => {
+app.post("/bhukkad/signup", (req, res) => {
 
-    const newUser = new userModel({
-        fullName: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        mobile: req.body.mobile
-    })
-    newUser.save(err => {
-        try {
-            if (!err) {
-                res.json({
-                    status: 201,
-                    message: "User created successfully"
-                })
-            }else {
-                throw err;
+    bcrypt.hash(req.body.password, saltRounds, (err, hashedPassword) => {
+        const newUser = new userModel({
+            fullName: req.body.fullName,
+            email: req.body.email,
+            password: hashedPassword,
+            mobile: req.body.mobile
+        });
+
+        newUser.save(err => {
+            try {
+                if (!err) {
+                    res.json({
+                        status: 201,
+                        message: "User created successfully"
+                    })
+                }else {
+                    throw err;
+                }
             }
-        }
-            catch (error) {
-                res.json({
-                    status: 404,
-                    message: err
-                })
-            }
+                catch (error) {
+                    res.json({
+                        status: 404,
+                        message: err.message
+                    })
+                }
+                
             
-        
+        });
     });
+});
+
+app.post("/bhukkad/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password
+
+    userModel.findOne({email: email}, (err, data) => {
+        if (!err) {
+            if (data) {
+                bcrypt.compare(password, data.password, (err, result) => {
+                    if (err) {
+                        res.json({
+                            status: 404,
+                            message: err
+                        })
+                    } else {
+                        if (result) {
+                            res.json({
+                                status: 200,
+                                message: "User authenticated"
+                            })
+                        } else {
+                            res.json({
+                                status: 403,
+                                message: "User not authenticated"
+                            })
+                        }
+                    }
+                })
+            }
+        } else {
+            console.log(err);
+        }
+    })
 });
 
 
